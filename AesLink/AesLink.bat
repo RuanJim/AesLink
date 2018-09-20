3409111b0d793852391c68cf3bd373771c651bd2
Jim <jim.ruan@perkinelmer.com>
2018-09-20 08:41:57 +0800

    Add package builder files.

---
+++ AesLink/AesLink.bat
@@ -0,0 +1,6 @@
+"H:\Softwares\Spotfire 7.11\TIB_sfire_dev_7.11.0_win\SDK\Package Builder\Spotfire.Dxp.PackageBuilder-Console.exe"^
+ /pkdesc:"H:\GitRepos\AesLink\AesLink\AesLink\AesLink.pkdesc"^
+ /target:"H:\Build\AesLink\AttivioSearch_1.0.0.0.spk"^
+ /packageversion:1.0.0.0^
+ /basefolder:"H:\GitRepos\AesLink\AesLink\AesLink\bin\Debug"^
+ /refpath:"H:\Softwares\Spotfire 7.9\TIB_sfire_dev_7.9.0_win\SDK\Binaries"
